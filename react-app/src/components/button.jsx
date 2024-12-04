d38a60beb350012435e2ccb54e181b895ef9fe0b
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-04 03:47:59 +0200

    React <> Edit: Button Component

--- react-app/src/components/button.jsx
+++ react-app/src/components/button.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Button.css";
 
-const Button = ()=>{
-    return(
-        <button className={className} onClick={onClick} type={type}>
-            {children}
-        </button>
-    );
+const Button = ({ onClick, children, type = "button", className="button" }) => {
+  return (
+    <button className={className} onClick={onClick} type={type}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
