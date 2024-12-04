8d5aa2bf509ee0344f865020b6a2c9203352e8ae
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-04 13:51:42 +0200

    React <> Create: Quiz Component

---
+++ react-app/src/pages/Quiz.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useLocation } from 'react-router-dom';
+import GeneralQuiz from "../components/GeneralQuiz";
+import SportQuiz from "../components/SportQuiz";
+import TechQuiz from "../components/TechQuiz";
+
+const Quiz = ()=>{
+
+    const location = useLocation();
+    const data = location.state;
+
+
+    let path = "No Selected Path"
+    if(data === "general"){
+        path = <GeneralQuiz/>
+    } else if (data === "sports"){
+        path = <SportQuiz/>
+    } else if (data === "tech"){
+        path = <TechQuiz/>
+    }
+
+    return(
+        <>
+            <h1>Quiz Page</h1>
+            {/* <p>{(data === "general" ? console.log("hello") : (data === "sports" ? console.log("hello 2") : console.log("hello 3")))}</p> */}
+            <p>{path}</p>
+        </>
+    );
+};
+
+export default Quiz;
