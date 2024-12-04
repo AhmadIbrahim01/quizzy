8bcb4eb4e7e150b043c9e0aaf69a03762678a146
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-04 03:48:57 +0200

    React <> Edit: Some Stylings

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import './styles/base/utilities.css'
+import './styles/base/base.css'
+import Home from './pages/Home'
 
 function App() {
 
     return (
         <>
+            <Home></Home>
         </>
     )
 }
