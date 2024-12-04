4eed9aae3ae74dd9a1cef1dfa71a2456d72ab5f4
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-04 13:49:59 +0200

    React <> Add: React Routes

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,12 +2,27 @@ import './App.css'
 import './styles/base/utilities.css'
 import './styles/base/base.css'
 import Home from './pages/Home'
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Quiz from './pages/Quiz';
 
 function App() {
 
     return (
         <>
-            <Home></Home>
+            <Router>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home />}
+					>
+                    </Route>
+                    <Route
+                        path="/quiz"
+                        element={<Quiz />}
+					>
+                    </Route>
+                </Routes>
+            </Router>
         </>
     )
 }
