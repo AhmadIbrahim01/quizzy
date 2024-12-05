62abc58f98a39e6f02cd317041e6a590f66142c3
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-05 02:11:27 +0200

    React <> Edit: General Quiz Component

--- react-app/src/components/GeneralQuiz.jsx
+++ react-app/src/components/GeneralQuiz.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import QuizCard from "./QuizCard";
+import { general_questions } from "../assets/data.js"
 
 const GeneralQuiz = () =>{
     return(
         <div>
-            <h1>General Quiz</h1>
+            <h1>GENERAL QUIZ</h1>
+            <QuizCard questions={general_questions} />
         </div>
     )
 }
