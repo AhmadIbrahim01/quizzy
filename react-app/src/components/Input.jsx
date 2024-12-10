d0826d71b3d3d5ddc1498d9ea931c6934dbe76a7
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-10 03:28:45 +0200

    React <> Edit: Input Component

--- react-app/src/components/Input.jsx
+++ react-app/src/components/Input.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Input.css"
 
 
-const Input = ({ type, name, className = "input", placeholder, value, onChange , checked, required}) => {
+const Input = ({ type, name, className = "input", placeholder, value, onChange , checked, required="false"}) => {
 
     return(
-        <input type={type} name={name} className={className} placeholder={placeholder} value={value} onChange={onChange} checked={checked} required={required}/>
+        <input type={type} name={name} className={className} placeholder={placeholder} value={value} onChange={onChange} checked={checked}/>
     );
 
 };
