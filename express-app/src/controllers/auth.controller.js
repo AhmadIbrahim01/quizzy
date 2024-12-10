3ba3d46e2f695c3cc1e76895dd7b27a8019fc798
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-10 04:57:21 +0200

    Express <> Add: Register Method Inside Auth Controller

---
+++ express-app/src/controllers/auth.controller.js
@@ -0,0 +1,41 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt"
+
+export const register = async (req, res) =>{
+    const {username, password, name} = req.body;
+
+    try {
+        if(!username || !password){
+            return res.status(500).send({
+                message: "Incomplete Credentials",
+            });
+        };
+
+        const salt = await bcrypt.genSalt(10);
+        const hashed = await bcrypt.hash(password, 10);
+
+        const user = User.create({
+            username,
+            password:hashed,
+            name,
+        })
+
+        return res.send(user);
+
+
+    } catch (error) {
+        console.log(error.message);
+
+        return res.status(500).send({
+            message: "Error Happened",
+        });
+    };
+
+
+
+}
+
+
+export const login = (req, res) =>{
+
+}
