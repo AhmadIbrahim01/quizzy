68363d3ff428b75e3fa1d7d2d63ed444ae2f8021
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-10 23:23:29 +0200

    Express <> Edit: auth.controller.js

--- express-app/src/controllers/auth.controller.js
+++ express-app/src/controllers/auth.controller.js
@@ -2,77 +2,63 @@ import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const register = async (req, res) =>{
-
-    const {username, password, name} = req.body;
+export const register = async (req, res) => {
+    const { username, password, name } = req.body;
 
     try {
-        if(!username || !password){
-            return res.status(500).send({
+        if (!username || !password) {
+            return res.status(400).send({
                 message: "Incomplete Credentials",
             });
-        };
+        }
 
         const salt = await bcrypt.genSalt(10);
         const hashed = await bcrypt.hash(password, salt);
-        
+
         const user = await User.create({
             username,
             password: hashed,
             name,
-        })
-
-        return res.status(201).send({
-            user
         });
 
-
+        return res.status(201).send({
+            user,
+        });
     } catch (error) {
         console.log(error.message);
-
         return res.status(500).send({
             message: "Error Happened",
         });
-    };
+    }
+};
 
-
-
-}
-
-
-export const login = async (req, res) =>{
-    const {username, password} = req.body;
+export const login = async (req, res) => {
+    const { username, password } = req.body;
 
     try {
-        
-        const user = await User.findOne({
-            username
-        });
-        
-        
-        if(!user){
-            return res.status(404).send({
-                message: "Invalid Credentials"
-            })
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(401).send({
+                message: "Invalid Credentials",
+            });
         }
 
         const check = await bcrypt.compare(password, user.password);
 
-        if(!check){
-            return res.status(400).send({
-                message: "Invalid Credentials"
-            })
+        if (!check) {
+            return res.status(401).send({
+                message: "Invalid Credentials",
+            });
         }
 
-
-        const token = await jwt.sign({ userId: user.id }, "secret");
+        const token = jwt.sign({ userId: user.id, name: user.name }, "secret");
 
         return res.status(200).send({ user, token });
     } catch (error) {
         console.log(error.message);
-
         return res.status(500).send({
             message: "Error Happened",
         });
-    };
-}
+    }
+};
